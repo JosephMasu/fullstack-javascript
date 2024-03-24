359cf514c6407fb6597215c3c1b3dfbf50ec8588
Joe Masu <josephmuhindo089@gmail.com>
2024-03-24 01:48:03 -0700

    Declaration & Expression functions

---
+++ 04 Function/02 Declaration & Expression/app.js
@@ -0,0 +1,13 @@
+// Function Declaration
+function sayHello(username) {
+    console.log(`Hello ${username}`);
+  }
+  
+  sayHello("HuXn");
+  
+  // Function Expressions
+  const greetings = function (user) {
+    console.log(`Hello ${user}`);
+  };
+  
+  greetings("Doe");
