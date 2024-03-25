16979ef7fa60636d7091cb14b3d9e57977efd91b
Joe Masu <josephmuhindo089@gmail.com>
2024-03-25 05:57:59 -0700

    Arrow Funtion javascript

---
+++ Part 2/02 Arrow function/app.js
@@ -0,0 +1,44 @@
+// function greet (username){
+//     return `HELLO ${username}`
+// }
+// console.log(greet`Masu`);
+
+// greet = username =>{
+//     return `HELLO ${username}`;
+// };
+// console.log(greet`Masu`);
+
+
+// REFACTOR THIS CODE TO ES6 ARROW FUNCTIONS.
+
+// setTimeout(function () {
+//   console.log("Hello");
+//   setTimeout(function () {
+//     console.log("Hey");
+//     setTimeout(function () {
+//       console.log("Namaste");
+//       setTimeout(function () {
+//         console.log("Hi");
+//         setTimeout(function () {
+//           console.log("Bonjour");
+//         }, 2000);
+//       }, 2000);
+//     }, 2000);
+//   }, 2000);
+// }, 2000);
+
+setTimeout( () => {
+  console.log("Hello");
+  setTimeout( () => {
+    console.log("Hey");
+    setTimeout( () =>{
+      console.log("Sallam");
+      setTimeout( () =>{
+        console.log("Hi");
+        setTimeout( () =>{
+          console.log("Bonjour");
+        }, 2000);
+      }, 2000);
+    }, 2000);
+  }, 2000);
+}, 2000);
