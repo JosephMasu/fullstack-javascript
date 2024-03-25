80f6a224e88ab55b25c04437e394c0007112362d
Joe Masu <josephmuhindo089@gmail.com>
2024-03-25 05:28:46 -0700

    Template string in javascrip

---
+++ Part 2/01 Template string/app.js
@@ -0,0 +1,20 @@
+// 1. Store this in template string ~
+// The quick
+// brown fox
+// jumps over
+// the lazy dog
+
+// 2. Create two variables (firstName, lastName)
+// 3. Use that variables in template string.
+
+let text = `
+The quick
+brown fox
+jumps over
+the lazy dog
+`;
+let firstName = `Masu`;
+let lastName = `MUHINDO`;
+//console.log(text);
+
+console.log(`My name is: ${firstName} ${lastName}`);
