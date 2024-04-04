c913a3e49bda92f98756ec110ab9b60c5103e193
Joe Masu <josephmuhindo089@gmail.com>
2024-04-04 06:32:06 -0700

    Object.create

---
+++ Part 5 Advanced/04 Object.create/app.js
@@ -0,0 +1,19 @@
+// The Object.create() method creates a new object, using an existing object as the prototype of the newly created object.
+
+let person = {
+    greet: function () {
+      console.log(`Hello my name is ${this.firstName} ${this.lastName}`);
+    },
+  };
+  
+  const masu = Object.create(person);
+  masu.firstName = "Masu";
+  masu.lastName = "Muhindo";
+  masu.greet();
+  
+  let Gaga = Object.create(person, {
+    firstName: { value: "Plumes" },
+    lastName: { value: "Galien" },
+  });
+  
+  Gaga.greet();
