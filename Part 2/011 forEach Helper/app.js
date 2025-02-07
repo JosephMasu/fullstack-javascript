8f7a149751ad91d65c4c4b68fec28bcfb19ed531
Joseph Masu <60499662+JosephMasu@users.noreply.github.com>
2025-02-07 22:05:25 +0200

    Update app.js

--- Part 2/011 forEach Helper/app.js
+++ Part 2/011 forEach Helper/app.js
@@ -7,7 +7,6 @@ function adder(num){
 numbers.forEach(adder);
 console.log(sum);
 
-
 // Understanding the Differences: forEach vs map in JavaScript
 const namess = ["Rohit","Aakash","Vinay","Ashish","Vasu"];
 
